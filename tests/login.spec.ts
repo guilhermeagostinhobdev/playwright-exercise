69bd97db6cc5331baf9c1a391af98a0c811fdace
Guilherme <guilherme.agostinho@bairesdev.com>
2024-08-08 09:16:32 -0600

    New tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -24,7 +24,7 @@ test('the user logs in with success', async ({ page }) => {
     await expect(productTitle).toHaveText('Products');
 });
 
-test('the user adds an item to the cart', async ({ page }) => {
+test('the user adds an item to the cart from the main page', async ({ page }) => {
     await login({ page });
 
     const addCart = page.locator('//div[@id="inventory_container"]/div/div[1]/div[contains(@class, "pricebar")]/button');
@@ -38,6 +38,35 @@ test('the user adds an item to the cart', async ({ page }) => {
     await expect(cartLabel).toHaveText('Your Cart');
 });
 
+test('the user adds an item to the cart from the item detail page', async ({ page }) => {
+    await login({ page });
+    await itemDetail ({ page });
+    
+    const addItemButton = page.locator('[class="btn_primary btn_inventory"]');
+    await addItemButton.click();
+
+    await page.locator('[data-icon="shopping-cart"]').click();
+
+    expect(page.url()).toBe('https://www.saucedemo.com/v1/cart.html');
+    
+    const cartLabel = await page.locator(('//*[@id="contents_wrapper"]/div[2]'));
+    await expect(cartLabel).toHaveText('Your Cart');
+
+});
+
+test('the user sees the detail of an item', async ({ page }) => {
+    await login({ page });
+
+    const item = page.locator('//*[@id="item_4_title_link"]/div');
+    await item.click();
+
+    await expect(page.locator('[class="inventory_details_name"]')).toBeVisible();
+    await expect(page.locator('[class="inventory_details_price"]')).toBeVisible();
+    await expect(page.locator('[class="inventory_details_desc"]')).toBeVisible();
+});
+
+
+
 async function login({ page }) {
     await page.goto('https://www.saucedemo.com/v1/');
     await expect(await page.title()).toBe('Swag Labs');
@@ -45,4 +74,11 @@ async function login({ page }) {
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[class="btn_action"]').click();
+}
+
+async function itemDetail({ page }) {
+    await login({ page });
+
+    const item = page.locator('//*[@id="item_4_title_link"]/div');
+    await item.click();
 }
